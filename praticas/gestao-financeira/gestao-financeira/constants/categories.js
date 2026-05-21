104e096743e661b3fb1a88221868ceedfdec0f29
DanFonR <dannyelfr@yahoo.com.br>
2026-05-21 08:52:08 -0300

    feat: Seletores de categorias e datas (Projeto03)

---
+++ praticas/gestao-financeira/gestao-financeira/constants/categories.js
@@ -0,0 +1,36 @@
+import colors from "./colors"
+
+const categories = Object.freeze({
+    income: {
+        icon: "work",
+        background: colors.categoryIncome,
+        name: "income",
+        displayName: "Renda"
+    },
+    food: {
+        icon: "fastfood",
+        background: colors.categoryFood,
+        name: "food",
+        displayName: "Alimentação"
+    },
+    house: {
+        icon: "home",
+        background: colors.categoryHouse,
+        name: "house",
+        displayName: "Casa"
+    },
+    education: {
+        icon: "book",
+        background: colors.categoryEducation,
+        name: "education",
+        displayName: "Educação"
+    },
+    travel: {
+        icon: "airplanemode-active",
+        background: colors.categoryTravel,
+        name: "travel",
+        displayName: "Viagens"
+    }
+});
+
+export default categories;
