66c3f0967d076a3436ce406894ffb71b39f1c24f
DanFonR <dannyelfr@yahoo.com.br>
2026-06-05 09:07:45 -0300

    fix: Fix de orreção de import não salvo

--- praticas/gestao-financeira/gestao-financeira-api/src/middlewares/auth.js
+++ praticas/gestao-financeira/gestao-financeira-api/src/middlewares/auth.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import { StatusCodes } from "http-status-codes";
-//import {Request, Response, NextFunction} from "express";
+import express from "express";
+const {Request, Response, NextFunction} = express;
 
 /**
  * @description Verifica token JWT e barra requisições sem token ou com token inválido/expirado
